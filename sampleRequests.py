8bcad6a65b9e9c84c34cda1a8de1ccbbd0088bd9
Rido Atmanto <ridoatmanto@gmail.com>
2021-06-26 23:24:15 +0700

    sample using package 'BeautifulSoap4'

--- sampleRequests.py
+++ sampleRequests.py
@@ -1,11 +1,16 @@
 import requests as requests
+from bs4 import BeautifulSoup
 
 url = 'https://cnnindonesia.com'
 try:
     request = requests.get(url)
     if (request.status_code == 200):
         print('success', request.status_code)
-        print('content', request.text)
+        # print('content', request.text)
+        soup = BeautifulSoup(request.text, features="html.parser")
+        print(f'Request Result for {url}')
+        print(f'Title Tag: {soup.title}')
+        print(f'Title : {soup.title.string}')
     else:
         print('failed', request.status_code)
 except Exception as e:
